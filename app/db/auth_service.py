029fc37e2ebd52a1408a6c311edf2dbdba78236e
Anthony <77089408+acasas00@users.noreply.github.com>
2026-02-06 00:34:35 -0500

    Enhance register_user to include more user details
    Expanded the register_user function to include additional user details such as first name, last name, email, phone number, address, and zip code.

--- app/db/auth_service.py
+++ app/db/auth_service.py
@@ -7,21 +7,29 @@ db = get_database()
 
 users = db["users"]
 
-def register_user(username, password):
+def register_user(username, password, first_name, last_name, email, phone_num, address, zip_code):
     if users.find_one({"username": username}):
-
         return False
 
     users.insert_one({"username": username,
-                     "password":hash_password(password) })
+                     "password":hash_password(password),
+                      "first_name": first_name,
+                      "last_name": last_name,
+                      "email": email,
+                      "phone_num": phone_num,
+                      "address": address,
+                      "zip_code": zip_code
+                      })
     return True
 
+#User login
 def login_user(username, password):
     user = users.find_one({"username": username})
     if not user:
         return False
 
     return verify_password(password, user["password"])
+
 
 
 
