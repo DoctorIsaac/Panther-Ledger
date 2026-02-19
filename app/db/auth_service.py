b1640359fe41367b30cef3b8d1955af3a2ae531c
Anthony <77089408+acasas00@users.noreply.github.com>
2026-02-19 11:22:29 -0500

    Enhance user registration with unique user_number
    Added user_number to registration process using next_counter for unique identification.

--- app/db/auth_service.py
+++ app/db/auth_service.py
@@ -1,17 +1,20 @@
 from .db_connection import get_database
 from .pass_util import  hash_password, verify_password
+from .counters import next_counter
 
-
-#Connect to Database
 db = get_database()
-
 users = db["users"]
 
 def register_user(username, password, first_name, last_name, email, phone_num, address, zip_code):
     if users.find_one({"username": username}):
         return False
 
-    users.insert_one({"username": username,
+    user_number = next_counter("user_id", start=0)
+    user_number_str = f"({user_number:08d})"
+
+    users.insert_one({
+                     "user_number": user_number_str,
+                     "username": username,
                      "password":hash_password(password),
                       "first_name": first_name,
                       "last_name": last_name,
@@ -22,13 +25,16 @@ def register_user(username, password, first_name, last_name, email, phone_num, a
                       })
     return True
 
-#User login
 def login_user(username, password):
     user = users.find_one({"username": username})
     if not user:
         return False
 
     return verify_password(password, user["password"])
+
+
+
+
 
 
 
