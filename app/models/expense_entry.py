d70782e3fc29898eaee733e6bfb1a07bef00d2fb
Anthony <77089408+acasas00@users.noreply.github.com>
2026-02-19 23:07:02 -0500

    Implement update and delete for expense entries
    Added functionality to update and delete expense entries. Updated existing expense entry checks to include 'is_active' status.

--- app/models/expense_entry.py
+++ app/models/expense_entry.py
@@ -17,7 +17,7 @@ def create_expense_entry(user_id: str, name: str, amount: Decimal, category_ref:
 
 #Checks if expense has already been previously entered
 #Needs to be fixed in case of two identical purchases at same time
-    existing_expense = expenses.find_one({"user_id": user_obj_id, "name": name_clean, "amount": amount_clean})
+    existing_expense = expenses.find_one({"user_id": user_obj_id, "name": name_clean, "amount": float(amount_clean),"is_active": True})
     if existing_expense:
         raise ValueError("Expense already exists")
 
@@ -40,6 +40,7 @@ def create_expense_entry(user_id: str, name: str, amount: Decimal, category_ref:
                 "name": name_clean,
                 "amount": float(amount_clean),
                 "description": description.strip(),
+                "is_active": True,
                 "purchase_date": date_clean,
                 "created_at": datetime.now(timezone.utc)
     }
@@ -50,5 +51,71 @@ def create_expense_entry(user_id: str, name: str, amount: Decimal, category_ref:
 
 #TODO
 #Edit Entry
-#Delete Entry
-#Assign Entry to Doc
+def update_expense_entry(user_id: str, category_ref: str, amount: Decimal, expense_id: int, name: str, purchase_date: str ="", description: str = ""):
+    user_obj_id = ObjectId(user_id)
+
+    name_clean = name.strip().lower()
+    amount_clean = Decimal(amount).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+    date_clean = date_parser(purchase_date)
+
+    existing_expense = expenses.find_one({
+                                "user_id": user_obj_id,
+                                "name": name_clean,
+                                "amount": float(amount_clean),
+                                "expense_id": {"$ne": expense_id},
+                                "is_active": True
+                                })
+    if existing_expense:
+        raise ValueError("Expense already exists")
+
+    category_obj_id = ObjectId(category_ref)
+    category_doc = categories.find_one({
+        "_id": category_obj_id,
+        "user_id": user_obj_id,
+        "is_active": True, })
+
+    if not category_doc:
+        raise ValueError("Category does not exist")
+
+    result = expenses.update_one({
+                                "user_id": user_obj_id,
+                                "expense_id": expense_id,
+                                "is_active": True},
+                                {"$set":{
+                                "name": name_clean,
+                                "category_ref": category_obj_id,
+                                "amount": float(amount_clean),
+                                "description": description.strip(),
+                                "purchase_date": date_clean,
+                                "updated_at": datetime.now(timezone.utc)}}
+                                )
+
+    if result.matched_count == 0:
+        raise ValueError("Expense does not exist")
+
+    return result.modified_count == 1
+
+def delete_expense_entry(user_id: str,expense_id: int):
+    user_obj_id = ObjectId(user_id)
+
+    existing = expenses.find_one({
+        "user_id": user_obj_id,
+        "expense_id": expense_id,
+        "is_active": True}
+    )
+    if not existing:
+        raise ValueError("Expense does not exist")
+
+    result = expenses.update_one(
+        {"user_id": user_obj_id,
+         "expense_id": expense_id,
+         "is_active": True},
+        {"$set":
+             {"is_active": False,
+              "updated_at": datetime.now(timezone.utc)}
+         })
+
+    if result.matched_count == 0:
+        raise ValueError("Expense does not exist")
+
+    return True
