be86819e323a64c30f063eb2a444104a46b3fe96
Anthony <77089408+acasas00@users.noreply.github.com>
2026-03-11 14:52:05 -0400

    Update date_parser to use YYYY-MM-DD format

--- app/models/util.py
+++ app/models/util.py
@@ -3,11 +3,11 @@ from datetime import datetime, timezone
 def date_parser(date_str: str):
 
     if not date_str or not date_str.strip():
-        return datetime.now(timezone.utc)
+        return datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
 
     try:
-        date = datetime.strptime(date_str.strip(), "%m-%d-%Y")
+        date = datetime.strptime(date_str.strip(), "%Y-%m-%d")
         return date.replace(tzinfo=timezone.utc)
 
     except ValueError:
-        raise ValueError("Incorrect date format")
+        raise ValueError("Date must be in YYYY-MM-DD format")
